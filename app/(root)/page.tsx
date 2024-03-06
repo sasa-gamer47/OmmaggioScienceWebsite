12c3dc296d42187e06b314f8ca329895f0e647b1
sasa-gamer47 <giacomofilippi237@gmail.com>
2024-03-06 17:03:50 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <nav className="fixed top-0 left-0 h-20 w-screen bg-gray-200 shadow-sm">
+        
+      </nav>
+
+    </main>
+  );
+}
---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export type CreateUserParams = {
+    clerkId: string
+    username: string
+    email: string
+    photo: string
+}
+
+export type UpdateUserParams = {
+    username: string
+    photo: string
+}
